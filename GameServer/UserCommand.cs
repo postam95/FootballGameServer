c3dc3fdc6643df4201a75240a3fd74a433007734
postam95 <postam95@gmail.com>
2021-05-01 12:31:14 +0200

    Labdavezetes

--- GameServer/UserCommand.cs
+++ GameServer/UserCommand.cs
@@ -35,7 +35,11 @@ namespace GameServer
 
         public void doCommand()
         {
-            if (!isMovingValid())
+            if (kick)
+                doKicking();
+
+            // Ha a mozgás nem fér bele a játéktérbe, akkor nem hajtódik végre.
+            if (!isPlayerMovingValid())
                 return;
 
             if (clientId == 1)
@@ -52,7 +56,7 @@ namespace GameServer
             }
         }
 
-        public bool isMovingValid()
+        private bool isPlayerMovingValid()
         {
             GameState gameState = SingletonGameState.GetInstance().GetGameState();
             if (clientId == 1)
@@ -78,6 +82,64 @@ namespace GameServer
                     return false;
             }
             return true;
+        }
+
+        private void doKicking()
+        {
+            GameState gameState = SingletonGameState.GetInstance().GetGameState();
+            double playerDistanceToBall;
+
+            // Ha a játékos nincs megfelelő távoláson belül a labdához, akkor a rúgás nem hajtódik végre.
+            if (!isKickingValid())
+                return;
+
+            if (clientId == 1)
+            {
+                double moveX = gameState.PictureBallX - gameState.PictureHomePlayer1X;
+                double moveY = gameState.PictureBallY - gameState.PictureHomePlayer1Y;
+                playerDistanceToBall = calculateDistance(gameState.PictureHomePlayer1X, gameState.PictureHomePlayer1Y,
+                                                         gameState.PictureBallX, gameState.PictureBallY);
+                gameState.PictureBallX += (Int32)(2 * moveX / playerDistanceToBall);
+                gameState.PictureBallY += (Int32)(2 * moveY / playerDistanceToBall);
+            }
+            else
+            {
+                double moveX = gameState.PictureBallX - gameState.PictureAwayPlayer1X;
+                double moveY = gameState.PictureBallY - gameState.PictureAwayPlayer1Y;
+                playerDistanceToBall = calculateDistance(gameState.PictureAwayPlayer1X, gameState.PictureAwayPlayer1Y,
+                                                         gameState.PictureBallX, gameState.PictureBallY);
+                gameState.PictureBallX += (Int32)(2 * moveX / playerDistanceToBall);
+                gameState.PictureBallY += (Int32)(2 * moveY / playerDistanceToBall);
+            }
+        }
+
+        private bool isKickingValid()
+        {
+            GameState gameState = SingletonGameState.GetInstance().GetGameState();
+            double playerDistanceToBall;
+            if (clientId == 1)
+            {
+                playerDistanceToBall = calculateDistance(gameState.PictureHomePlayer1X, gameState.PictureHomePlayer1Y,
+                                                         gameState.PictureBallX,        gameState.PictureBallY);
+                if (playerDistanceToBall > 80.0)
+                    return false;
+            }
+            else
+            {
+                playerDistanceToBall = calculateDistance(gameState.PictureHomePlayer1X, gameState.PictureHomePlayer1Y,
+                                                         gameState.PictureBallX,        gameState.PictureBallY);
+                if (playerDistanceToBall > 80.0)
+                    return false;
+            }
+            return true;
+        }
+
+        private double calculateDistance(double firstX, double firstY, double secondX, double secondY)
+        {
+            double moveX = firstX - secondX;
+            double moveY = firstY - secondY;
+            double distance = Math.Sqrt(Math.Pow(moveX, 2) + Math.Pow(moveY, 2));
+            return distance;
         }
 
         public bool Up { get => up; set => up = value; }
